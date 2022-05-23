b18f5d4b402e7baf82b0ed626a4bd93475821ff6
AleAndino <aleandino.cba@gmail.com>
2022-05-23 16:01:17 -0300

    homework IntroToCS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,11 +3,32 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+  let sumatoria = 0;
+  let numero = num.toString();
+  let posicion = numero.length - 1;
+  
+  for (let i = 0; i < numero.length; i++) {
+    if (posicion === -1) {
+      break;
+    }
+    sumatoria += numero[i] * 2 ** posicion;
+    posicion--;
+  }
+  return sumatoria;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+  let binario = [];
+  
+  while (num !== 0) {
+    let division = Math.floor(num / 2);
+    let resto = num % 2;
+    num = division;
+    binario.push(resto); 
+  }
+  return binario.reverse().join("");
 }
 
 
